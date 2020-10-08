835dfb5504e6fd2988ec8a8547ffc44801fba0aa
Leon Zhou <leon.zhou1@foodstuffs.co.nz>
2020-10-09 11:42:31 +1300

    update

--- demo-api/build.gradle.kts
+++ demo-api/build.gradle.kts
@@ -29,6 +29,7 @@ dependencies {
     api(project(":demo-base"))
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-hibernate5")
     implementation("io.arrow-kt:arrow-core:$arrowVersion")
---
+++ demo-api/src/main/kotlin/com/github/b1412/demo/config/Health.kt
@@ -0,0 +1,7 @@
+package com.github.b1412.demo.config
+
+import org.springframework.context.annotation.Bean
+
+class Health {
+
+}
